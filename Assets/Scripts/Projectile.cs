48dbd33315838635249050a6ef0e9f904fff234d
hoodey <41493055+hoodey@users.noreply.github.com>
2024-04-05 21:10:04 -0500

    Finished Custom Weapon
    Added a custom weapon called EZPZ3000 with sound and particle effects.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,11 +5,11 @@ using UnityEngine.Events;
 
 public class Projectile : MonoBehaviour
 {
-    float damageAmount;
-    float speed;
-    float knockback;
-    float lifetime;
-    UnityAction<HitData> OnHit;
+    protected float damageAmount;
+    protected float speed;
+    protected float knockback;
+    protected float lifetime;
+    protected UnityAction<HitData> OnHit;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +23,7 @@ public class Projectile : MonoBehaviour
         
     }
 
-    public void Initialize(float damage, float velocity, float life, float force, UnityAction<HitData> onHit)
+    public virtual void Initialize(float damage, float velocity, float life, float force, UnityAction<HitData> onHit)
     {
         damageAmount = damage;
         speed = velocity;
